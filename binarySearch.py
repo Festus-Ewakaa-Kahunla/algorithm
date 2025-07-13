cb0ecb290748999d1254129aedcd8dda5c4f4a42
Festus-Ewakaa-Kahunla <kahunlafestusewakaa2000@gmail.com>
2025-07-13 07:12:23 -0400

    binary search

---
+++ binarySearch.py
@@ -0,0 +1,14 @@
+def binarySearch(arr, target):
+    "Implementation of a binary search algorithm"
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == target:
+            return mid
+        elif guess > target:
+            high = mid - 1
+        elif guess < target:
+            low = mid + 1
+        return None
