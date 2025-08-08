b9aabd7a1b64730b6672183d64fd6d5d005875c0
Festus-Ewakaa-Kahunla <kahunlafestusewakaa2000@gmail.com>
2025-08-08 14:06:11 -0400

    testing c

---
+++ C/anot.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* Print farenheit and celsius table */
+main()
+{
+    int F, C;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    F = lower;
+    while (F <= upper)
+    {
+        C = 5 * (F - 32) / 9;
+        printf("%3d %6d\n", F, C);
+        F += step;
+    }
+}
--- Fundamentals/dynamic_array/dynamic.py
+++ Fundamentals/dynamic_array/dynamic.py
@@ -5,5 +5,14 @@ class DynamicArray:
         self.fixed_array = [None] * self.capacity
 
     def get(self, i):
-        if i < 0 or i >= self._size():
-            
+        if i < 0 or i >= self._size:
+            raise IndexError("Index out of bounds")
+        return self.fixed_array[i]
+    
+    def set(self, i, x):
+        if i < 0 or i >= self._size:
+            raise IndexError("Index out of bounds")
+        self.fixed_array[i] = x
+
+    def size(self):
+        return self._size
