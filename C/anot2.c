7954fb2bf43166386bf0b3188a33ffcb3af3fee5
Festus-Ewakaa-Kahunla <kahunlafestusewakaa2000@gmail.com>
2025-08-25 19:59:03 -0400

    copy sequence of characters

--- C/anot2.c
+++ C/anot2.c
@@ -5,4 +5,7 @@ main()
     {
         printf("%2.1f %5.1f\n", f, (5.0 / 9.0) * (f - 32));
     }
+
+#define LOWER 0
+#define UPPER 300
 }
--- Fundamentals/array/copy.c
+++ Fundamentals/array/copy.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+// copy character in a sequence, by copy and print.
 main()
 {
     int c;
     c = getchar();
+
     while (c != EOF)
     {
         putchar(c);
