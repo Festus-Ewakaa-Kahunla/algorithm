2451ff418f2ef7ebd46b7ceb9d49ecead1891f42
Festus-Ewakaa-Kahunla <kahunlafestusewakaa2000@gmail.com>
2025-09-05 12:05:13 -0400

    encrypt file

---
+++ Programs/IncomeTax/taxRate.py
@@ -0,0 +1,16 @@
+pay = int(input("Enter you pay: "))
+duration = int(input("""
+            Duration:
+                     1) Hourly
+                     2) Weekly
+                     3) Biweekly
+                     4) Monthly
+                     5) Yearly
+                     """))
+state = int(input("""
+            State:
+                  1) East
+                  2) West
+                  3) North
+                  4) South
+                  """))
